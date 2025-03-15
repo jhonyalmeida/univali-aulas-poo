3ef2e64f7e0e08c8432c4b5d58da7b84932138e0
Jhony Luiz de Almeida <jhonyl.almeida@gmail.com>
2025-03-15 19:22:19 -0300

    - first commit

---
+++ src/main/java/br/univali/aula02/ArcoFlecha.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.univali.aula02;
+
+public class ArcoFlecha implements Atirador {
+
+    @Override
+    public void atirar() {
+        System.out.println("atirando flechas!!!");
+    }
+    
+}
